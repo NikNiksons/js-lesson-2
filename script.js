b52eff3e82bb6897f9b973b760751f0a631f96b6
NikNiksons <123307796+NikNiksons@users.noreply.github.com>
2023-03-01 13:24:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,156 @@
+
+
+// Завдання 1
+
+// let count = 0;
+// let ask1 = prompt('За скільки днів Бог створив світ?');
+// if (ask1 == 7) {
+//     console.log('Вірна відповідь!')
+//     count++;
+// }
+// else if (ask1 > 7 || ask1 < 7) {
+//     console.log('Не вірно (за 7 днів)');
+// }
+
+// let ask2 = prompt('Дружина Адама?');
+// if (ask2.toLowerCase() === 'єва' || ask2.toLowerCase() === 'eva') {
+//     console.log('Вірно! Молодець!');
+//     count++;
+// }
+// else if (ask2) {
+//     console.log('Не вірно (Єва). Ви можите знайти підказки в Біблії Синодальний переклад');
+// }
+
+// let ask3 = prompt('Брат Каїна?');
+// if (ask3.toLowerCase() === 'авель' || ask3.toLowerCase() === 'avel') {
+//     console.log('Вірно! Так тримати!');
+//     count++;
+// }
+// else if (ask3) {
+//     console.log('Не вірно (Авель). Це діти Адама та Єви; Книга Буття');
+// }
+
+// let ask4 = prompt('Хто побудував ковчег?');
+// if (ask4.toLowerCase() === 'ной' || ask4.toLowerCase() === 'noj') {
+//     console.log('Вірно! Так тримати!');
+//     count++;
+// }
+// else if (ask4) {
+//     console.log('Не вірно (Ной). Був потоп, Бог врятував його та його сімю');
+// }
+
+// let ask5 = prompt('Який був знак, що потопу більше не буде?');
+// if (ask5.toLowerCase() === 'веселка' || ask5.toLowerCase() === 'veselka') {
+//     console.log('Вірно!');
+//     count++;
+// }
+// else if (ask5) {
+//     console.log('Не вірно (Веселка). Бог пообіцяв людям, що більше ніколи не знищить землю ПОТОПОМ і знаком є веселка в небі');
+// }
+// let ask6 = prompt('Хто вивів Ізраїльський народ з Єгипту?');
+// if (ask6.toLowerCase() === 'мойсей' || ask6.toLowerCase() === 'moisey') {
+//     console.log('Вірно!');
+//     count++;
+// }
+// else if (ask6) {
+//     console.log('Не вірно (Мойсей).');
+// }
+
+// let ask7 = prompt('Наймудріший цар Ізраїлю?');
+// if (ask7.toLowerCase() === 'соломон' || ask7.toLowerCase() === 'solomon') {
+//     console.log('Вірно!');
+//     count++;
+// }
+// else if (ask7) {
+//     console.log('Не вірно (Соломон).');
+// }
+
+// let ask8 = prompt('У якій річці хрестили Ісуса Христа?');
+// if (ask8.toLowerCase() === 'йордан' || ask8.toLowerCase() === 'iordan') {
+//     console.log('Вірно!');
+//     count++;
+// }
+// else if (ask8) {
+//     console.log('Не вірно (Йордан)');
+// }
+
+// let ask9 = prompt('Невіруючий ученик Ісуса Христа?');
+// if (ask9.toLowerCase() === 'фома' || ask9.toLowerCase() === 'foma') {
+//     console.log('Вірно!');
+//     count++;
+// }
+// else if (ask9) {
+//     console.log('Не вірно (Фома)');
+// }
+// let ask10 = prompt('Євангеліє – це?');
+// if (ask10.toLowerCase() === 'блага вість' || ask10.toLowerCase() === 'добра вість') {
+//     console.log('Вірно!');
+//     count++;
+// }
+// else if (ask10) {
+//     console.log('Не вірно (Блага вість)');
+// }
+// console.log('Сount=', count);
+
+// if (count >= 0 && count <= 2) {
+//     console.log('дуже погано');
+// }
+// else if (count >= 2 && count <= 5) {
+//     console.log('погано');
+// }
+// else if (count >= 5 && count <= 8) {
+//     console.log(' добре, треба ще трішки почитати');
+// }
+// else if (count >= 8 && count <= 9) {
+//     console.log('Гарний рівень знань');
+// }
+// else if (count === 10) {
+//     console.log('Відмінно!! Вітаємо!!!');
+// }
+
+
+// Завдання 2
+
+// let ask = prompt("Введіть Ім'я");
+// if (ask == "Ім'я") {
+//     let password = prompt('Введіть пароль');
+//     if (password == 'ЛОГОС') {
+//         console.log('Ласкаво просимо!');
+//     }
+//     else if (password > '0') {
+//         console.log('Пароль невірний')
+//     }
+//     else {
+//         console.log('Вхід скасовано');
+//     }
+// }
+
+// else if (ask > '0') {
+//     console.log('Я вас не знаю');
+// }
+// else {
+//     console.log('Вхід скасовано');
+// }
+
+// Завдання 3
+
+// let numeric = prompt('Введіть число ');
+// if (numeric >= 1 && numeric <= 2 || numeric == 12) {
+//     alert('Зима');
+// }
+// else if (numeric >= 3 && numeric <= 5) {
+//     alert('Весна');
+// }
+// else if (numeric >= 6 && numeric <= 8) {
+//     alert('Літо');
+// }
+// else if (numeric >= 9 && numeric <= 11) {
+//     alert('Осінь');
+// }
+
+// else if (numeric) {
+//     alert('Неможливо');
+// }
+
+
+
